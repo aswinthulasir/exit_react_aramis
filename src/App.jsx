55d2d788713b8c283c0b43176caa0d6a2ac4e91e
aswinthulasir <aswinthulasi007@gmail.com>
2025-03-12 09:39:03 +0530

    test-commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [cin, setCin]=useState('')
+  const [caddin,setCaddin]=useState('')
+  const [couts,setCouts]=useState('')
+
+
+  const checkIn = () => {
+    
+    setCin(counter + 1);
+};
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
